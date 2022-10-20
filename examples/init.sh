61d56f11ba2db4af15ff53b3c7fd3e7cd461b2c5
sparsh-anurag <78465897+sparsh-anurag@users.noreply.github.com>
2022-10-20 19:31:16 +0530

    Add files via upload

--- examples/init.sh
+++ examples/init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 yum update -y
-cat << EOF > /etc/yum.repos.d/mongodb-org-6.0.repo
-[mongodb-org-6.0]
+cat << EOF > /etc/yum.repos.d/mongodb-org-5.0.repo
+[mongodb-org-5.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/5.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
 EOF
 
 yum install -y mongodb-org -y
+systemctl start mongod
+systemctl enable mongod
 # mongosh
-
