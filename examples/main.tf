1216afdd0eea462c8ebd7a51f8fba18ec5e0c693
sparsh-anurag <78465897+sparsh-anurag@users.noreply.github.com>
2022-11-02 12:52:19 +0530

    updated module and example

---
+++ examples/main.tf
@@ -0,0 +1,23 @@
+
+module "ec2-mongo" {
+    source                  = "../"
+    vpc_id                  = "vpc-01f1488876c4a803e"
+    key_name                = "sparsh-nv"
+    instance_type           = "t2.micro"
+    iam_instance_profile    = "ssm-role"
+    security_groups         = ["sg-0a967f4fa886231f5 "]
+    subnet_id               = "subnet-0b45bc874e97793e4"
+    ebs_optimized           = false
+    disable_api_termination = true
+    disable_api_stop        = true
+    delete_on_termination   = true
+    source_dest_check       = true
+    encrypted               = true
+    volume_type             = "gp3"
+    root_volume_size        = "10"
+    project_name_prefix       = "dev-test"
+
+    common_tags = {
+    "Feature" : "application"
+    }
+}
--- main.tf
+++ main.tf
@@ -1,28 +1,41 @@
 data "aws_ami" "amazon-linux-2" {
     most_recent = true
     filter {
-        name   = "owner-alias"
-        values = ["amazon"]
+      name   = "owner-alias"
+      values = ["amazon"]
     }
     filter {
-        name   = "name"
-        values = ["amzn2-ami-hvm*"]
+      name   = "name"
+      values = ["amzn2-ami-hvm*"]
     }
+    owners = ["amazon"]
 }
 
-module "ec2-mongo" {
-    source                  = "./modules/ec2-ebs"
-    vpc_id                  = var.vpc_id
-    ami_id                  = data.aws_ami.amazon-linux-2.id
-    key_name                = var.key_name
-    instance_type           = var.instance_type
-    iam_instance_profile    = var.iam_instance_profile
-    security_groups         = var.security_groups
-    user_data_script        = file("userdata.sh")
-    subnet_id               = var.subnet_id
-    ebs_optimized           = var.ebs_optimized
-    disable_api_termination = var.disable_api_termination
-    volume_type             = var.volume_type
-    root_volume_size        = var.root_volume_size
-    tags                    = merge(local.common_tags, tomap({ "Name" : "${local.project_name_prefix}-mongo-db" }))
+data "template_file" "user_data" {
+    template = file("${path.module}/userdata.sh")
+}
+
+resource "aws_instance" "ec2" {
+	ami                     = data.aws_ami.amazon-linux-2.id
+	instance_type           = var.instance_type
+	subnet_id               = var.subnet_id
+	vpc_security_group_ids  = var.security_groups
+	key_name                = var.key_name
+	iam_instance_profile    = var.iam_instance_profile
+	ebs_optimized           = var.ebs_optimized
+	disable_api_termination = var.disable_api_termination
+	user_data_base64        = base64encode(data.template_file.user_data.rendered)
+	source_dest_check       = var.source_dest_check
+    disable_api_stop        = var.disable_api_stop
+
+	volume_tags = merge(var.common_tags, tomap({ "Name" : "${var.project_name_prefix}-mongodb" }))
+	tags        = merge(var.common_tags, tomap({ "Name" : "${var.project_name_prefix}-mongodb" }))
+
+	root_block_device {
+		delete_on_termination = var.delete_on_termination
+		encrypted             = var.encrypted
+		volume_size           = var.root_volume_size
+		volume_type           = var.volume_type
+	}
+
 }
