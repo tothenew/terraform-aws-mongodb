49239e705640403305a43094dc6bb17acfc330cf
Ketan Joshi <ketanjoshi@Ketans-MacBook-Pro.local>
2022-12-05 18:51:25 +0530

    Removed Key pair creation and profile

--- example/main.tf
+++ example/main.tf
@@ -7,4 +7,6 @@ module "mongodb" {
   mongo_subnet_id     = "subnet-0569ea294831bb782"
   jumpbox_subnet_id   = "subnet-0b740611c69644d90"
   mongo_database      = "admin"
+  jumpbox_access_cidr = "0.0.0.0/0"
+  key_name = "devops"
 }
